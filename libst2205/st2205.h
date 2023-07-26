3199ad6194c24a222dbf23151d9740ccc8696a1a
Grazfather <grazfather@gmail.com>
2023-07-26 19:09:52 -0400

    v1.3a

--- libst2205/st2205.h
+++ libst2205/st2205.h
@@ -1,3 +1,4 @@
+//Handle definition for the st2205_* routines
 typedef struct {
     int fd;
     int width;
@@ -8,7 +9,12 @@ typedef struct {
     char* oldpix;
 } st2205_handle;
 
+//Opens the device pointed to by dev (which is /dev/sdX) and reads its 
+//capabilities.
 st2205_handle *st2205_open(char *dev);
+//Close and free the info associated with h
 void st2205_close(st2205_handle *h);
+//Send an array of h->width*h->height r,g,b triplets
 void st2205_send_data(st2205_handle *h,char *pixinfo);
+//Turn the backlight on or off
 void st2205_backlight(st2205_handle *h, int on);
